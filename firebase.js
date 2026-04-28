e3a3123bd982598bfe4d9f41a3914568e2885f09
Mauro91689 <sebastianpesoa13@gmail.com>
2026-04-28 02:04:48 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,26 +1,17 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-firestore.js";
 
-// Tu config
 const firebaseConfig = {
   apiKey: "AIzaSyAUIYpYNUOflr6bEtYxshHwlhKZ_bM3G2k",
   authDomain: "ticketuc-9ceed.firebaseapp.com",
   projectId: "ticketuc-9ceed",
   storageBucket: "ticketuc-9ceed.firebasestorage.app",
   messagingSenderId: "810543370297",
-  appId: "1:810543370297:web:7482d921082606df7efde8",
-  measurementId: "G-70HMXQ4WRY"
+  appId: "1:810543370297:web:ac9e320a07836c0a7efde8",
+  measurementId: "G-GK5BKSXD7Y"
 };
 
-// Inicializar
 const app = initializeApp(firebaseConfig);
-
-// Servicios que vas a usar
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
-
 const db = getFirestore(app);
 
 export { db };
