050d23432e9dbd858c4030da7442d5373e6e7517
Mauro91689 <sebastianpesoa13@gmail.com>
2026-04-28 01:58:31 -0300

    Update main.js

--- main.js
+++ main.js
@@ -8,3 +8,20 @@ createUserWithEmailAndPassword(auth, "test@test.com", "123456")
   .catch((error) => {
     console.log("Error:", error.message);
   });
+import { db } from "./firebase.js";
+import { collection, addDoc } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+async function probarFirebase() {
+  try {
+    const docRef = await addDoc(collection(db, "prueba"), {
+      nombre: "Mauro",
+      mensaje: "Funciona Firebase 🚀"
+    });
+
+    console.log("Documento guardado con ID:", docRef.id);
+  } catch (e) {
+    console.error("Error:", e);
+  }
+}
+
+probarFirebase();
